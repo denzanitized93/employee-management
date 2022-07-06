d200e9cce2d53610f343c04f77060a634f7c7676
denzanitized93 <denzanitized93@gmail.com>
2022-07-07 00:38:16 +0800

    employee management
    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,87 @@
+<script setup>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="border border-blue shadow rounded-md p-4 w-full h-full mx-auto">
+                            <div class="animate-pulse flex space-x-4">
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 bg-slate-200 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-200 rounded"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="animate-pulse flex space-x-4 mt-2">
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 bg-slate-200 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-200 rounded"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="animate-pulse flex space-x-4 mt-2">
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 bg-slate-200 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-200 rounded"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="animate-pulse flex space-x-4 mt-2">
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 bg-slate-200 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-200 rounded"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="animate-pulse flex space-x-4 mt-2">
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 bg-slate-200 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-200 rounded"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
